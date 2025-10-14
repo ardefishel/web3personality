386a1dc69e6198b637666a9ee2dbead6f8ed634d
rtvcl // wira <ardefishel@RTVCL---Macbook-Pro-M4.local>
2025-10-14 16:59:04 +0700

    feat: ignition deployment

---
+++ contracts/ignition/modules/deployment.ts
@@ -0,0 +1,22 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+export default buildModule("QuizModuleProject0001",  (m) => {
+
+  const MINTER_ROLE = "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"; // keccak256 generated  
+  
+  const personalityTokenContract = m.contract("PersonalityToken",["https://ipfs.io/ipfs/xxx/"]);
+
+  const quizManagerContract = m.contract("QuizManager", [personalityTokenContract], {
+    after: [personalityTokenContract]
+  })
+
+  m.call(personalityTokenContract, "grantRole", [MINTER_ROLE, quizManagerContract], {
+    id: "grantMinterRoleQuizContract0001"
+  });
+
+  return { 
+    personalityTokenContract,
+    quizManagerContract
+   };
+});
