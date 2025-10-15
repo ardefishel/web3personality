85cc10a245428ed99203990399a75d445d3dc5d4
rtvcl // wira <ardefishel@RTVCL---Macbook-Pro-M4.local>
2025-10-15 14:55:04 +0700

    fix: farcaster import

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
+  ssr: false
 });
 
 function RouteComponent() {
